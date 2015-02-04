39c49323337ade01cafc29dca163502135b227f8
Yuichi Shiwaku <yuichi.shiwaku@gmail.com>
2015-02-04 11:00:51 +0900

    tutorial 16

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -65,11 +65,23 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.refs.author.getDOMNode().value.trim();
+        var text = this.refs.text.getDOMNode().value.trim();
+        if (!text || !author) {
+            return;
+        }
+        // TODO: send request to the server
+        this.refs.author.getDOMNode().value = '';
+        this.refs.text.getDOMNode().value = '';
+        return;
+    },
     render: function () {
         return (
-            <form className="commentForm">
-                <input type="text" placeholder="Your name" />
-                <input type="text" placeholder="Say something..." />
+            <form className="commentForm" onsubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" ref="author" />
+                <input type="text" placeholder="Say something..." ref="text" />
                 <input type="submit" value="Post" />
             </form>
         );
